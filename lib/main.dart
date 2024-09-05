7a16a15befc77d6da420c9b35686a4c37b04c05e
Hasan Batal <hh0657768@gmail.com>
2024-09-05 09:25:13 -0700

    call setup method in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:game_store/core/constants/strings.dart';
 import 'package:game_store/core/theme/themes.dart';
+import 'package:game_store/di/service_locator.dart';
 import 'package:game_store/features/home/domain/entities/device.dart';
 import 'package:game_store/router/app_router.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   runApp(const GameStoreApp());
+  setup();
   await Hive.initFlutter();
   Hive.registerAdapter(DeviceEntityAdapter());
   await Hive.openBox(kDeviceKeyBox);
