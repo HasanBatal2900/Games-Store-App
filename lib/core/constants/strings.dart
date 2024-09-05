226d2f2101463c856b3d16fef2db716b812f100e
Hasan Batal <hh0657768@gmail.com>
2024-09-05 09:24:46 -0700

    add some messages

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,3 +1,11 @@
 const kAudioPath = "audios/start.mp3";
 const kDeviceKeyBox = "deviceKeyBox";
 const kEmptyCacheErrorMessage = "There is no Data right Now";
+const kAddDeviceSucessMessage = 'Device Added Successfuly';
+const kDeleteDeviceSucessMessage = 'Device Deleted Successfuly';
+const kUpdateDeviceSucessMessage = 'Device Updated Successfuly';
+const kGetAllDeviceSucessMessage = 'Device Fetched Successfuly';
+const kGetAllDeviceFailureMessage = 'Device Fetched Failed';
+const kAddDeviceFailureMessage = 'Device Added Failed';
+const kDeleteDeviceFailureMessage = 'Device Deleted Failed';
+const kUpdateDeviceFailureMessage = 'Device Updated Failed';
